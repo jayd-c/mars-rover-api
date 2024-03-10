68c763cd300c3b68ff2f4ca854e2919367f10f38
JayanDesilva <jaycdesilva@gmail.com>
2024-03-10 14:43:45 -0400

    Controller added

--- src/main/java/com/marsroverapi/showphotos/HomeController.java
+++ src/main/java/com/marsroverapi/showphotos/HomeController.java
@@ -1,2 +1,26 @@
-package com.marsroverapi.showphotos;public class HomeController {
+package com.marsroverapi.showphotos;
+
+import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home (ModelMap model) {
+        model.put("name","Jayan Desilva");
+        model.put("address","12124 Shadow Street");
+        model.put("email","jesusKappiya@gmail.com");
+        model.put("id","100");
+        return "index";
+    }
+
+    @GetMapping("/testing")
+    public String testing (ModelMap model) {
+        model.put("id","100");
+        return "test";
+    }
 }
