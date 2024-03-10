0edad962f561405411b937bbdb476438a61f946e
JayanDesilva <jaycdesilva@gmail.com>
2024-03-09 21:22:54 -0500

    Initial commit

---
+++ src/main/java/com/marsroverapi/showphotos/ShowphotosApplication.java
@@ -0,0 +1,13 @@
+package com.marsroverapi.showphotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowphotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowphotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marsroverapi/showphotos/ShowphotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marsroverapi.showphotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowphotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
